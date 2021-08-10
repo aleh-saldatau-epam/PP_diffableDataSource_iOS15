1e70fadace44ee11b598c1933023970db56822d7
Aleh Saldatau <Aleh_Saldatau@epam.com>
2021-08-10 15:09:14 +0300

    Initial Commit

---
+++ PP_diffableDataSource_iOS15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PP_diffableDataSource_iOS15
+//
+//  Created by Oleg Soldatoff on 10.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
